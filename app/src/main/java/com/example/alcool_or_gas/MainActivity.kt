92c0b57385bc373d2c6515162be7cc8585976190
Andravi <andravi75@gmail.com>
2024-12-25 17:29:11 -0300

    Refactored project structure

--- app/src/main/java/com/example/alcool_or_gas/MainActivity.kt
+++ app/src/main/java/com/example/alcool_or_gas/MainActivity.kt
@@ -10,29 +10,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,15 +26,18 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.alcool_or_gas.ui.composables.FuelInput
+import com.example.alcool_or_gas.ui.composables.ResButton
+import com.example.alcool_or_gas.ui.composables.ResultadoText
+import com.example.alcool_or_gas.ui.composables.Switch70Or75
+import com.example.alcool_or_gas.ui.composables.TopBar
 import com.example.alcool_or_gas.ui.theme.Alcool_or_gasTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,12 +56,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun priceValidator(text: String): Boolean {
+fun priceValidator(text: String): Boolean {
     val pattern = Regex("^\\d+(,\\d*)?\$")
     return (text.isEmpty() || text.matches(pattern)) && text.count { it == ',' } <= 1
 }
 
-private fun isAlcoolBetter(a: Double, g: Double, factor: Float): Boolean {
+fun isAlcoolBetter(a: Double, g: Double, factor: Float): Boolean {
     val rent = a / g
     println(rent)
     if (rent <= factor) {
@@ -169,7 +158,7 @@ fun App(modifier: Modifier = Modifier) {
                     Switch70Or75(checked, onSwitched = { checked = it })
                 }
                 ResultadoText(res)
-                resButton {
+                ResButton {
                     res = isAlcoolBetter(
                         alcoolText.let {
                             if (it.isNotEmpty()) {
@@ -195,122 +184,7 @@ fun App(modifier: Modifier = Modifier) {
 }
 
 
-@Composable
-fun resButton(onResActi: () -> Unit) {
-    Button(
-        onClick = { onResActi() },
-        enabled = true,
-        contentPadding = PaddingValues(42.dp, 12.dp),
-        modifier = Modifier.padding(top = 40.dp)
-    ) {
-        Text(
-            "CALCULAR",
-            color = MaterialTheme.colorScheme.tertiary,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            letterSpacing = 1.sp,
-        )
-    }
-}
 
-
-@Composable
-fun Switch70Or75(checked: Boolean, onSwitched: (Boolean) -> Unit) {
-    Text(
-        "70%",
-        fontWeight = FontWeight.Bold,
-        color = MaterialTheme.colorScheme.onSurface
-    )
-    Switch(
-        checked = checked,
-        onCheckedChange = { che -> onSwitched(che) },
-        thumbContent = if (checked) {
-            {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
-                    contentDescription = null,
-                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                )
-            }
-        } else {
-            {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = null,
-                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                )
-            }
-        }
-    )
-    Text(
-        "75%",
-        fontWeight = FontWeight.Bold,
-        color = MaterialTheme.colorScheme.onSurface
-    )
-}
-
-
-@Composable
-fun ResultadoText(bool: Boolean?) {
-    if (bool == null) {
-        return
-    }
-    if (bool) {
-        Text(
-            text = "Compensa",
-            fontStyle = FontStyle.Italic,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-    } else {
-        Text(
-            text = "Não compensa",
-            fontStyle = FontStyle.Italic,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-
-    }
-}
-
-@Composable
-fun FuelInput(t: String, onChangeText: (String) -> Unit) {
-    var text = t
-
-    TextField(
-        value = text, // The current text value
-        onValueChange = {
-            text = if (priceValidator(it)) {
-                it
-            } else {
-                it.substring(0, it.length - 1)
-            }
-            onChangeText(text)
-        },
-        placeholder = {
-            Text(
-                "Digite o preço do litro, Ex: 5,40",
-                color = MaterialTheme.colorScheme.onSurface,
-                fontWeight = FontWeight.Thin
-            )
-        },
-        leadingIcon = {
-            Text(
-                "R$ ",
-                color = MaterialTheme.colorScheme.secondary,
-                fontWeight = FontWeight.Bold
-            )
-        },
-        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-        colors = TextFieldDefaults.colors(
-            cursorColor = MaterialTheme.colorScheme.onSurface,
-            unfocusedContainerColor = Color.Transparent,
-            focusedContainerColor = Color.Transparent,
-            focusedTextColor = MaterialTheme.colorScheme.onSurface,
-            unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
-            focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
-        )
-    )
-
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -318,19 +192,4 @@ fun AppPreview() {
     Alcool_or_gasTheme {
         App()
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopBar() {
-    TopAppBar(colors = topAppBarColors(
-        containerColor = MaterialTheme.colorScheme.primary,
-        titleContentColor = MaterialTheme.colorScheme.tertiary,
-    ), title = {
-        Text(
-            "Fuel Insight", style = MaterialTheme.typography.labelLarge.copy(
-                color = MaterialTheme.colorScheme.tertiary
-            )
-        )
-    })
 }
---
+++ app/src/main/java/com/example/alcool_or_gas/ui/composables/components.kt
@@ -0,0 +1,160 @@
+package com.example.alcool_or_gas.ui.composables
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.alcool_or_gas.priceValidator
+
+@Composable
+fun ResButton(onResActi: () -> Unit) {
+    Button(
+        onClick = { onResActi() },
+        enabled = true,
+        contentPadding = PaddingValues(42.dp, 12.dp),
+        modifier = Modifier.padding(top = 40.dp)
+    ) {
+        Text(
+            "CALCULAR",
+            color = MaterialTheme.colorScheme.tertiary,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            letterSpacing = 1.sp,
+        )
+    }
+}
+
+@Composable
+fun Switch70Or75(checked: Boolean, onSwitched: (Boolean) -> Unit) {
+    Text(
+        "70%",
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+    Switch(
+        checked = checked,
+        onCheckedChange = { che -> onSwitched(che) },
+        thumbContent = if (checked) {
+            {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            }
+        } else {
+            {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            }
+        }
+    )
+    Text(
+        "75%",
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+}
+
+
+@Composable
+fun ResultadoText(bool: Boolean?) {
+    if (bool == null) {
+        return
+    }
+    if (bool) {
+        Text(
+            text = "Compensa",
+            fontStyle = FontStyle.Italic,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    } else {
+        Text(
+            text = "Não compensa",
+            fontStyle = FontStyle.Italic,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+
+    }
+}
+
+@Composable
+fun FuelInput(t: String, onChangeText: (String) -> Unit) {
+    var text = t
+
+    TextField(
+        value = text, // The current text value
+        onValueChange = {
+            text = if (priceValidator(it)) {
+                it
+            } else {
+                it.substring(0, it.length - 1)
+            }
+            onChangeText(text)
+        },
+        placeholder = {
+            Text(
+                "Digite o preço do litro, Ex: 5,40",
+                color = MaterialTheme.colorScheme.onSurface,
+                fontWeight = FontWeight.Thin
+            )
+        },
+        leadingIcon = {
+            Text(
+                "R$ ",
+                color = MaterialTheme.colorScheme.secondary,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+        colors = TextFieldDefaults.colors(
+            cursorColor = MaterialTheme.colorScheme.onSurface,
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            focusedTextColor = MaterialTheme.colorScheme.onSurface,
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+            focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
+        )
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(colors = topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        titleContentColor = MaterialTheme.colorScheme.tertiary,
+    ), title = {
+        Text(
+            "Fuel Insight", style = MaterialTheme.typography.labelLarge.copy(
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        )
+    })
+}
