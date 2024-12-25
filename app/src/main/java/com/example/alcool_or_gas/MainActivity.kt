4cc296ecae3b809d79f24cb923e13d6b8b9f905a
Andravi <andravi75@gmail.com>
2024-12-25 17:17:13 -0300

    Refactored components

--- app/src/main/java/com/example/alcool_or_gas/MainActivity.kt
+++ app/src/main/java/com/example/alcool_or_gas/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -26,8 +25,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -39,14 +36,12 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -64,7 +59,7 @@ class MainActivity : ComponentActivity() {
             Alcool_or_gasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     App(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -72,34 +67,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private fun priceValidator(text: String): Boolean {
+    val pattern = Regex("^\\d+(,\\d*)?\$")
+    return (text.isEmpty() || text.matches(pattern)) && text.count { it == ',' } <= 1
+}
 
-fun isAlcoolBetter(a: Double, g: Double, factor: Float): Boolean {
+private fun isAlcoolBetter(a: Double, g: Double, factor: Float): Boolean {
     val rent = a / g
     println(rent)
     if (rent <= factor) {
         return true
     }
-
     return false
 }
 
 
-fun calcular(alcoolText: String, gasText: String, checked: Boolean) {
-    Log.d("testes", "Funciona")
-
-
-}
-
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun App(name: String, modifier: Modifier = Modifier) {
+fun App(modifier: Modifier = Modifier) {
 
     var alcoolText by rememberSaveable { mutableStateOf("") }
     var gasText by rememberSaveable { mutableStateOf("") }
     var checked by rememberSaveable { mutableStateOf(false) }
     var res: Boolean? by rememberSaveable { mutableStateOf(null) }
 
-    Scaffold(topBar = { topbar() }) {
+    Scaffold(topBar = { TopBar() }) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceAround,
@@ -117,6 +109,7 @@ fun App(name: String, modifier: Modifier = Modifier) {
                     .padding(0.dp, 16.dp)
             ) {
                 Spacer(modifier = Modifier.size(0.dp, 48.dp))
+
                 Image(
                     painter = painterResource(id = R.drawable.fuel_insight_logo_removebg),
                     contentDescription = "some thing",
@@ -124,6 +117,7 @@ fun App(name: String, modifier: Modifier = Modifier) {
                         200.dp
                     )
                 )
+
                 Text(
                     text = "Qual o Combustível mais vantajoso ?",
                     fontSize = 25.sp,
@@ -132,12 +126,15 @@ fun App(name: String, modifier: Modifier = Modifier) {
                     color = MaterialTheme.colorScheme.onSurface
 
                 )
+
                 Text(
                     text = "Preencha os valores e Calcule!",
                     fontStyle = FontStyle.Italic,
                     color = MaterialTheme.colorScheme.onSurface
                 )
+
                 Spacer(modifier = Modifier.size(0.dp, 32.dp))
+
                 Text(
                     text = "Preço da Gasolina:", fontSize = 22.sp,
                     fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onSurface,
@@ -148,39 +145,7 @@ fun App(name: String, modifier: Modifier = Modifier) {
 
                 Spacer(modifier = Modifier.size(0.dp, 16.dp))
 
-                TextField(
-                    value = gasText, // The current text value
-                    onValueChange = {
-                        gasText = if (priceValidator(it)) {
-                            it
-                        } else {
-                            it.substring(0, it.length - 1)
-                        }
-                    },
-                    placeholder = {
-                        Text(
-                            "Digite o preço do litro, Ex: 5,40",
-                            color = MaterialTheme.colorScheme.onSurface,
-                            fontWeight = FontWeight.Thin
-                        )
-                    },
-                    leadingIcon = {
-                        Text(
-                            "R$ ",
-                            color = MaterialTheme.colorScheme.secondary,
-                            fontWeight = FontWeight.Bold
-                        )
-                    },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    colors = TextFieldDefaults.colors(
-                        cursorColor = MaterialTheme.colorScheme.onSurface,
-                        unfocusedContainerColor = Color.Transparent,
-                        focusedContainerColor = Color.Transparent,
-                        focusedTextColor = MaterialTheme.colorScheme.onSurface,
-                        unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
-                        focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
-                    )
-                )
+                FuelInput(gasText, onChangeText = { gasText = it })
 
                 Text(
                     text = "Preço da Álcool:", fontSize = 22.sp,
@@ -192,112 +157,37 @@ fun App(name: String, modifier: Modifier = Modifier) {
 
                 Spacer(modifier = Modifier.size(0.dp, 24.dp))
 
-                TextField(
-                    value = alcoolText, // The current text value
-                    onValueChange = {
-                        alcoolText = if (priceValidator(it)) {
-                            it
-                        } else {
-                            it.substring(0, it.length - 1)
-                        }
-                    },
-                    placeholder = {
-                        Text(
-                            "Digite o preço do litro, Ex: 5,40",
-                            color = MaterialTheme.colorScheme.onSurface,
-                            fontWeight = FontWeight.Thin
-                        )
-                    },
-                    leadingIcon = {
-                        Text(
-                            "R$ ",
-                            color = MaterialTheme.colorScheme.secondary,
-                            fontWeight = FontWeight.Bold
-                        )
-                    },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    colors = TextFieldDefaults.colors(
-                        cursorColor = MaterialTheme.colorScheme.onSurface,
-                        unfocusedContainerColor = Color.Transparent,
-                        focusedContainerColor = Color.Transparent,
-                        focusedTextColor = MaterialTheme.colorScheme.onSurface,
-                        unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
-                        focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
-                    )
-                )
+                FuelInput(alcoolText, onChangeText = { alcoolText = it })
+
                 Spacer(modifier = Modifier.size(0.dp, 16.dp))
+
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier.size(width = 130.dp, 32.dp)
                 ) {
-                    Text(
-                        "70%",
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                    Switch(
-                        checked = checked,
-                        onCheckedChange = {
-                            checked = it
-                        },
-                        thumbContent = if (checked) {
-                            {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            }
-                        } else {
-                            {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            }
-                        }
-                    )
-                    Text(
-                        "75%",
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
+                    Switch70Or75(checked, onSwitched = { checked = it })
                 }
-                resultado(res)
-                Button(
-                    onClick = {
-                        res = isAlcoolBetter(
-                            alcoolText.let {
-                                if (it.isNotEmpty()) {
-                                    it.replace(',', '.').toDouble()
-                                } else {
-                                    0.0
-                                }
-                            },
-                            gasText.let {
-                                if (it.isNotEmpty()) {
-                                    it.replace(',', '.').toDouble()
-                                } else {
-                                    0.0
-                                }
-                            },
-                            if (checked) 0.75f else 0.70f
-                        )
-                        Log.d("teste", res.toString())
-                    },
-                    enabled = true,
-                    contentPadding = PaddingValues(42.dp, 12.dp),
-                    modifier = Modifier.padding(top = 40.dp)
-                ) {
-                    Text(
-                        "CALCULAR",
-                        color = MaterialTheme.colorScheme.tertiary,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp,
-                        letterSpacing = 1.sp,
+                ResultadoText(res)
+                resButton {
+                    res = isAlcoolBetter(
+                        alcoolText.let {
+                            if (it.isNotEmpty()) {
+                                it.replace(',', '.').toDouble()
+                            } else {
+                                0.0
+                            }
+                        },
+                        gasText.let {
+                            if (it.isNotEmpty()) {
+                                it.replace(',', '.').toDouble()
+                            } else {
+                                0.0
+                            }
+                        },
+                        if (checked) 0.75f else 0.70f
                     )
+                    Log.d("teste", "asd")
                 }
             }
         }
@@ -306,10 +196,9 @@ fun App(name: String, modifier: Modifier = Modifier) {
 
 
 @Composable
-
-fun CalculateButton(onClick: () -> Unit) {
+fun resButton(onResActi: () -> Unit) {
     Button(
-        onClick = { onClick() },
+        onClick = { onResActi() },
         enabled = true,
         contentPadding = PaddingValues(42.dp, 12.dp),
         modifier = Modifier.padding(top = 40.dp)
@@ -324,10 +213,45 @@ fun CalculateButton(onClick: () -> Unit) {
     }
 }
 
+
 @Composable
-fun resultado(bool: Boolean?) {
+fun Switch70Or75(checked: Boolean, onSwitched: (Boolean) -> Unit) {
+    Text(
+        "70%",
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+    Switch(
+        checked = checked,
+        onCheckedChange = { che -> onSwitched(che) },
+        thumbContent = if (checked) {
+            {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            }
+        } else {
+            {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            }
+        }
+    )
+    Text(
+        "75%",
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onSurface
+    )
+}
 
 
+@Composable
+fun ResultadoText(bool: Boolean?) {
     if (bool == null) {
         return
     }
@@ -347,131 +271,58 @@ fun resultado(bool: Boolean?) {
     }
 }
 
-//@Composable
-//fun CalculateButton(onClick: () -> Unit) {
-////    Button(
-////        onClick = {
-////            isAlcoolBetter(
-////                alcoolText.let {
-////                    if (it.isNotEmpty()) {
-////                        it.replace(',', '.').toDouble()
-////                    } else {
-////                        0.0
-////                    }
-////                },
-////                gasText.let {
-////                    if (it.isNotEmpty()) {
-////                        it.replace(',', '.').toDouble()
-////                    } else {
-////                        0.0
-////                    }
-////                },
-////                if (checked) 0.75f else 0.70f
-////            )
-////        },
-////        enabled = true,
-////        contentPadding = PaddingValues(42.dp, 12.dp),
-////        modifier = Modifier.padding(top = 40.dp)
-////    ) {
-////        Text(
-////            "Calcular",
-////            color = MaterialTheme.colorScheme.tertiary,
-////            fontWeight = FontWeight.Bold,
-////            fontSize = 16.sp
-////        )
-////    }
-//}
+@Composable
+fun FuelInput(t: String, onChangeText: (String) -> Unit) {
+    var text = t
 
-//@Composable
-//fun Switch70Or75() {
-//
-//
-//}
+    TextField(
+        value = text, // The current text value
+        onValueChange = {
+            text = if (priceValidator(it)) {
+                it
+            } else {
+                it.substring(0, it.length - 1)
+            }
+            onChangeText(text)
+        },
+        placeholder = {
+            Text(
+                "Digite o preço do litro, Ex: 5,40",
+                color = MaterialTheme.colorScheme.onSurface,
+                fontWeight = FontWeight.Thin
+            )
+        },
+        leadingIcon = {
+            Text(
+                "R$ ",
+                color = MaterialTheme.colorScheme.secondary,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+        colors = TextFieldDefaults.colors(
+            cursorColor = MaterialTheme.colorScheme.onSurface,
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            focusedTextColor = MaterialTheme.colorScheme.onSurface,
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+            focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
+        )
+    )
 
-
-private fun priceValidator(text: String): Boolean {
-    val pattern = Regex("^\\d+(,\\d*)?\$")
-    return (text.isEmpty() || text.matches(pattern)) && text.count { it == ',' } <= 1
 }
-
-
-//@Composable
-//fun AlcoolInput() {
-//
-//}
-
-//@Composable
-//fun GasInput(t: String) {
-//    var text = t
-//
-//
-//    TextField(
-//        value = text, // The current text value
-//        onValueChange = {
-//            text = if (priceValidator(it)) {
-//                it
-//            } else {
-//                it.substring(0, it.length - 1)
-//            }
-//        },
-//        placeholder = {
-//            Text(
-//                "Digite o preço do litro, Ex: 5,40",
-//                color = MaterialTheme.colorScheme.onSurface,
-//                fontWeight = FontWeight.Thin
-//            )
-//        },
-//        leadingIcon = {
-//            Text(
-//                "R$ ",
-//                color = MaterialTheme.colorScheme.secondary,
-//                fontWeight = FontWeight.Bold
-//            )
-//        },
-//        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-//        colors = TextFieldDefaults.colors(
-//            cursorColor = MaterialTheme.colorScheme.onSurface,
-//            unfocusedContainerColor = Color.Transparent,
-//            focusedContainerColor = Color.Transparent,
-//            focusedTextColor = MaterialTheme.colorScheme.onSurface,
-//            unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
-//            focusedIndicatorColor = MaterialTheme.colorScheme.tertiary
-//        )
-//    )
-//
-////    OutlinedTextField(
-////        value = text, // The current text value
-////        onValueChange = { newText -> text = newText },
-////        placeholder = {
-////            Text("Digite o preço do litro, Ex: 5,40")
-////        },
-////        prefix = {
-////            Text("R$ ", color = MaterialTheme.colorScheme.secondary, fontWeight = FontWeight.Bold)
-////        },
-////        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-////        colors = OutlinedTextFieldDefaults.colors(
-////            cursorColor = MaterialTheme.colorScheme.secondary,
-////            unfocusedBorderColor = MaterialTheme.colorScheme.secondary,
-////            focusedLabelColor = MaterialTheme.colorScheme.secondary,
-////            focusedTextColor = MaterialTheme.colorScheme.secondary,
-////            focusedBorderColor = MaterialTheme.colorScheme.secondary,
-////        ),
-////    )
-//
-//
-//}
 
 @Preview(showBackground = true)
 @Composable
 fun AppPreview() {
     Alcool_or_gasTheme {
-        App("Android")
+        App()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun topbar() {
+fun TopBar() {
     TopAppBar(colors = topAppBarColors(
         containerColor = MaterialTheme.colorScheme.primary,
         titleContentColor = MaterialTheme.colorScheme.tertiary,
